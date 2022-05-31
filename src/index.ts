b3e92f881e7faf8a890779c8918a05ac3172d3e2
Ossamoon <osamu950623@gmail.com>
2022-05-31 18:50:29 +0900

    update typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { createInterface } from "readline";
+class A {
+  static foo = 123;
+  static bar = this.foo * 2;
+  static {
+    console.log("bar is", this.bar);
+  }
+}
 
-const rl = createInterface({ input: process.stdin, output: process.stdout });
-
-rl.question("文字列を入力してください:", (line) => {
-  const num = Number(line);
-  const num2 = Number(true);
-  console.log(num + num2);
-  rl.close();
-});
+const obj = new A();
+console.log(A);
